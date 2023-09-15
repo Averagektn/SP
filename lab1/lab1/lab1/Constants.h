0cbf6639196275162e1ab80006f1202f0cbd37c9
Никита Глушаченко <rylonoboev2004@gmail.com>
2023-09-15 19:37:44 +0300

    IT FINALLY WORKS!!!

---
+++ lab1/lab1/lab1/Constants.h
@@ -0,0 +1,8 @@
+#pragma once
+
+namespace ProjConst {
+	const wchar_t PROJ_NAME[8] = L"Tracker";
+	const wchar_t WND_CAPTION[33] = L"THIS SPRITE IS MOVING OMG!!!!!!!";
+	const int WND_DEF_HEIGHT = 800;
+	const int WND_DEF_WIDTH = 600;
+}
---
+++ lab1/lab1/lab1/Source.cpp
@@ -0,0 +1,55 @@
+#include <Windows.h>
+#include "Constants.h"
+
+LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam);
+
+int APIENTRY WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow)
+{
+	WNDCLASSEX wcex; HWND hWnd; MSG msg;
+
+	wcex.cbSize = sizeof(WNDCLASSEX);
+	wcex.style = CS_DBLCLKS;
+	wcex.lpfnWndProc = WndProc;
+	wcex.cbClsExtra = 0;
+	wcex.cbWndExtra = 0;
+	wcex.hInstance = hInstance;
+	wcex.hIcon = LoadIcon(NULL, IDI_APPLICATION);
+	wcex.hCursor = LoadCursor(NULL, IDC_ARROW);
+	wcex.hbrBackground = HBRUSH(CreateSolidBrush(RGB(0, 0, 0)));
+	wcex.lpszMenuName = NULL;
+	wcex.lpszClassName = ProjConst::PROJ_NAME;
+	wcex.hIconSm = wcex.hIcon;
+
+	RegisterClassEx(&wcex);
+
+	hWnd = CreateWindow(ProjConst::PROJ_NAME, ProjConst::WND_CAPTION, WS_OVERLAPPEDWINDOW, 
+		CW_USEDEFAULT, CW_USEDEFAULT, ProjConst::WND_DEF_WIDTH, ProjConst::WND_DEF_HEIGHT, 
+		NULL, NULL, hInstance, NULL);
+
+	ShowWindow(hWnd, nCmdShow);
+	UpdateWindow(hWnd);
+
+	while (GetMessage(&msg, NULL, 0, 0))
+	{
+		TranslateMessage(&msg);
+		DispatchMessage(&msg);
+	}
+
+	return (int)msg.wParam;
+}
+
+LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
+{
+	switch (message)
+	{
+	case WM_NCLBUTTONDBLCLK:
+		MessageBox(hWnd, L"Hello, world", L"Message", MB_OK);
+		break;
+	case WM_DESTROY:
+		PostQuitMessage(0);
+		break;
+	default:
+		return DefWindowProc(hWnd, message, wParam, lParam);
+	}
+	return 0;
+}
